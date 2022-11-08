b74e1e22bfdca5b5067cb062a3602f1a8fd7c90d
Luana Lima <luana.arqeurb@gmail.com>
2022-11-08 13:00:53 +0000

    added more cities and updated the current time

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
-setInterval(function () {
+function updateCities() {
   let parisElement = document.querySelector("#paris");
-
   if (parisElement) {
     let parisDateElement = parisElement.querySelector(".date");
     let parisTimeElement = parisElement.querySelector(".time");
@@ -12,24 +11,98 @@ setInterval(function () {
       .tz("Europe/Paris")
       .format("h:mm:ss a");
   }
-}, 1000);
+
+  let sydneyElement = document.querySelector("#sydney");
+  if (sydneyElement) {
+    let sydneyDateElement = sydneyElement.querySelector(".date");
+    let sydneyTimeElement = sydneyElement.querySelector(".time");
+
+    sydneyDateElement.innerHTML = moment()
+      .tz("Australia/Sydney")
+      .format("MMMM Do, YYYY");
+    sydneyTimeElement.innerHTML = moment()
+      .tz("Australia/Sydney")
+      .format("h:mm:ss a");
+  }
+
+  let madridElement = document.querySelector("#madrid");
+  if (madridElement) {
+    let madridDateElement = madridElement.querySelector(".date");
+    let madridTimeElement = madridElement.querySelector(".time");
+
+    madridDateElement.innerHTML = moment()
+      .tz("Europe/Madrid")
+      .format("MMMM Do, YYYY");
+    madridTimeElement.innerHTML = moment()
+      .tz("Europe/Madrid")
+      .format("h:mm:ss a");
+  }
+
+  let saoPauloElement = document.querySelector("#sao-paulo");
+  if (saoPauloElement) {
+    let saoPauloDateElement = saoPauloElement.querySelector(".date");
+    let saoPauloTimeElement = saoPauloElement.querySelector(".time");
+
+    saoPauloDateElement.innerHTML = moment()
+      .tz("America/Sao_Paulo")
+      .format("MMMM Do, YYYY");
+    saoPauloTimeElement.innerHTML = moment()
+      .tz("America/Sao_Paulo")
+      .format("h:mm:ss a");
+  }
+
+  let losAngelesElement = document.querySelector("#los-angeles");
+  if (losAngelesElement) {
+    let losAngelesDateElement = losAngelesElement.querySelector(".date");
+    let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+
+    losAngelesDateElement.innerHTML = moment()
+      .tz("America/Los_Angeles")
+      .format("MMMM Do, YYYY");
+    losAngelesTimeElement.innerHTML = moment()
+      .tz("America/Los_Angeles")
+      .format("h:mm:ss a");
+  }
+}
 
 function updateCity(event) {
-  let cityTimezone = event.target.value;
-  if (cityTimezone === "current") {
-    cityTimezone = moment.tz.guess();
+  currentTimezone = event.target.value;
+  if (currentTimezone === "current") {
+    currentTimezone = moment.tz.guess();
   }
-  let cityName = cityTimezone.replace("_", " ").split("/")[1];
-  let cityTime = moment().tz(cityTimezone);
+  currentCity = currentTimezone.replace("_", " ").split("/")[1];
+  currentCityTime = moment().tz(currentTimezone);
+  updateHTML();
+}
 
-  let citiesElement = document.querySelector("#cities");
+function updateTime() {
+  currentCityTime = moment().tz(currentTimezone);
+  updateHTML();
+}
+
+function updateHTML() {
+  let citiesElement = document.querySelector(".current");
   citiesElement.innerHTML = `
-    <div class="city">
-        <h2>${cityName}</h2>
-        <div class="date">${cityTime.format("MMMM Do, YYYY")}</div>
-        <div class="time">${cityTime.format("h:mm:ss a")}</div>
+    <div class="current">
+        <h2>
+          This is the current time
+          <div>in ${currentCity}</div>
+        </h2>
+        <div class="current-time">${currentCityTime.format(
+          "h:mm:ss [<small>]A[</small>]"
+        )}</div>
+        <h3>${currentCityTime.format("MMMM Do, YYYY")}</h3>
     </div>`;
 }
+let currentTimezone = moment.tz.guess();
+let currentCity = currentTimezone.replace("_", " ").split("/")[1];
+let currentCityTime = moment().tz(currentTimezone);
+
+updateHTML(currentCity, currentCityTime);
 
 let citiesSelectElement = document.querySelector("#city");
 citiesSelectElement.addEventListener("change", updateCity);
+
+setInterval(updateTime, 1000);
+setInterval(updateCities, 1000);
+updateCities();
