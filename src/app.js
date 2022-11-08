42e364985081792cc8a389f4c8ead31489834d27
Luana Lima <luana.arqeurb@gmail.com>
2022-11-08 10:46:03 +0000

    added current location

--- src/app.js
+++ src/app.js
@@ -16,6 +16,9 @@ setInterval(function () {
 
 function updateCity(event) {
   let cityTimezone = event.target.value;
+  if (cityTimezone === "current") {
+    cityTimezone = moment.tz.guess();
+  }
   let cityName = cityTimezone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimezone);
 
