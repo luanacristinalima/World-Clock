cfc16cade166b3668dc6745336bcde7a680bd27b
Luana Lima <luana.arqeurb@gmail.com>
2022-11-08 10:37:29 +0000

    added the functionality for the select

--- src/app.js
+++ src/app.js
@@ -1,8 +1,32 @@
 setInterval(function () {
   let parisElement = document.querySelector("#paris");
-  let parisDateElement = parisElement.querySelector(".date");
-  let parisTimeElement = parisElement.querySelector(".time");
 
-  parisDateElement.innerHTML = moment().format("MMMM Do, YYYY");
-  parisTimeElement.innerHTML = moment().tz("Europe/Paris").format("h:mm:ss a");
+  if (parisElement) {
+    let parisDateElement = parisElement.querySelector(".date");
+    let parisTimeElement = parisElement.querySelector(".time");
+
+    parisDateElement.innerHTML = moment()
+      .tz("Europe/Paris")
+      .format("MMMM Do, YYYY");
+    parisTimeElement.innerHTML = moment()
+      .tz("Europe/Paris")
+      .format("h:mm:ss a");
+  }
 }, 1000);
+
+function updateCity(event) {
+  let cityTimezone = event.target.value;
+  let cityName = cityTimezone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimezone);
+
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+    <div class="city">
+        <h2>${cityName}</h2>
+        <div class="date">${cityTime.format("MMMM Do, YYYY")}</div>
+        <div class="time">${cityTime.format("h:mm:ss a")}</div>
+    </div>`;
+}
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
